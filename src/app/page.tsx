245d11e1ca93ca957147e9cbe2ca0cf8af393e31
Fernando Castelan <fercc1097@gmail.com>
2025-01-25 19:38:37 -0600

    add Kanaler name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <h1>Kanaler</h1>
         <h1>Proximamente</h1>
       </main>
     </div>
